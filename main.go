0010800925f5921ac6a5b9f4625da9f8411c133f
assilbekov <askhat.assilbekov@gmail.com>
2024-05-09 18:47:06 -0300

    implement chain responsibility

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import "chain-of-responsibility/pkg"
+
 func main() {
-	//
+	device1 := pkg.Device{Name: "Device-1"}
+	updateSvc := pkg.UpdateDataService{Name: "UpdateDataService-1"}
+	saveSvc := pkg.ServiceDataSave{}
+
+	device1.SetNext(&updateSvc)
+	updateSvc.SetNext(&saveSvc)
+
+	data := pkg.Data{}
+
+	device1.Execute(&data)
+	updateSvc.Execute(&data)
+	saveSvc.Execute(&data)
 }
